316904d9bd7ecde1904ed3675692d7ed5483836f
Trung Nguyen <t2307533@partner.coway.com>
2025-03-05 10:10:35 +0700

    Initial Commit

---
+++ TutorialTCA/TutorialTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialTCAApp.swift
+//  TutorialTCA
+//
+//  Created by sotatek on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TutorialTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
