a66852b7e7e18e73a05f10fd4926e921d53b7994
Trung Nguyen <thanhtrungnguyen1208@gmail.com>
2025-03-13 16:21:13 +0700

    Navigation

---
+++ TutorialTCA/Contact/AddContactView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+import ComposableArchitecture
+
+struct AddContactView: View {
+    @Perception.Bindable var store: StoreOf<AddContactFeature>
+    
+    var body: some View {
+        Form {
+            TextField("Name", text: $store.contact.name.sending(\.setName))
+            Button("Save") {
+                store.send(.saveButtonTapped)
+            }
+        }
+        .toolbar {
+            ToolbarItem {
+                Button("Cancel") {
+                    store.send(.cancelButtonTapped)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AddContactView(
+            store: Store(
+                initialState: AddContactFeature.State(
+                    contact: Contact( id: UUID(),name: "Blob")
+                )
+            ) {
+                AddContactFeature()
+            }
+        )
+    }
+}
